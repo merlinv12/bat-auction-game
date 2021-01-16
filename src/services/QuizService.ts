47a693293219315ecb423e991b776272b257c219
Anish Patel <apatel010@gmail.com>
2021-01-15 17:46:50 -0800

    added env var for api url

--- src/services/QuizService.ts
+++ src/services/QuizService.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosResponse } from "axios"
+import * as dotenv from 'dotenv';
+dotenv.config({path: '../../.env.local'})
 
-const baseUrl: string = "http://localhost:8000"
+
+const baseUrl: string = process.env.API_URL ?? '';
 
 // The generic for AxiosResponse get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R>;
 export const getListings = async (limit: number = 10): Promise<AxiosResponse<Listing[]>> => {
