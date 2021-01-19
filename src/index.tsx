479653106e0a0ce86d37868b02eb2e276e3f6577
Anish Patel <apatel010@gmail.com>
2021-01-19 14:37:46 -0800

    added font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import QuizStart from './components/QuizStart';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
